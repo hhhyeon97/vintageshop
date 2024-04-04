ea906343392033a11a685f7c0801acb5ee675931
jihyeon <kth083030@naver.com>
2024-04-04 23:56:32 +0900

    db데이터

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import Footer from './component/Footer';
 import Header from './component/Header';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  const fetchPost = async () => {
+    setIsLoading(true);
+    const url = 'http://localhost:3005/posts';
+    const response = await fetch(url);
+    const data = await response.json();
+    setIsLoading(false);
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div className="wrap">
       <Header />
-      <section>내용입니다</section>
+      <section>
+        내용입니다
+        <div>
+          {data?.map((item, index) => (
+            <div key={index}>{item.title}</div>
+          ))}
+        </div>
+      </section>
       <Footer />
     </div>
   );

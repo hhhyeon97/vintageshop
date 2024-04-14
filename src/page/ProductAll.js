8346b223969ba9cfc2c7d86cff17330c1841f60a
jihyeon <kth083030@naver.com>
2024-04-14 20:51:26 +0900

    콘솔에선잘찍히지만ui에서는상품이안보이는중

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -3,11 +3,16 @@ import { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Card from '../component/Card';
 // import Carousel from '../component/Carousel';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
+
   const getProducts = async () => {
-    let url = `http://localhost:3004/products`;
+    let searchQuery = query.get('q') || '';
+    console.log('쿼리값은?', searchQuery);
+    let url = `http://localhost:3004/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     // console.log('데이터', data);
@@ -16,17 +21,24 @@ const ProductAll = () => {
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
 
   return (
     <Container className="product-all-wrap">
       {/* <Carousel /> */}
       <Row xs={1} sm={2} md={2} lg={4} className="justify-content-center">
-        {productList.map((item) => (
-          <Col lg={3} className="d-flex justify-content-center mb-5 card-hover">
-            <Card item={item} />
-          </Col>
-        ))}
+        {productList.length === 0 ? (
+          <p>검색 결과가 없습니다.</p>
+        ) : (
+          productList.map((item) => (
+            <Col
+              lg={3}
+              className="d-flex justify-content-center mb-5 card-hover"
+            >
+              <Card item={item} />
+            </Col>
+          ))
+        )}
       </Row>
     </Container>
   );

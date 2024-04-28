80ff0b671b1faef34b199c248182dd861017dae9
minsol <kth083030@naver.com>
2024-04-28 21:35:08 +0900

    수정커밋

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,22 +6,31 @@ const Login = ({ setAuthenticate }) => {
   const navigate = useNavigate();
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const idRef = useRef(null);
+  const pwRef = useRef(null);
 
   const loginCheck = (event) => {
     event.preventDefault();
 
     if (id.trim() === '') {
       alert('아이디를 입력하세요!');
+      idRef.current.focus();
       return;
     }
     if (password.trim() === '') {
       alert('비밀번호를 입력하세요!');
+      pwRef.current.focus();
       return;
     }
 
     setAuthenticate(true);
-    // console.log('id/pw', id, password);
     navigate('/');
+  };
+
+  const handleFocus = (ref) => {
+    if (ref.current.value.trim() === '') {
+      ref.current.value = ''; // 입력란의 값 초기화
+    }
   };
 
   return (
@@ -44,16 +53,20 @@ const Login = ({ setAuthenticate }) => {
             placeholder="id"
             value={id}
             onChange={(e) => setId(e.target.value)}
+            ref={idRef}
+            onFocus={() => handleFocus(idRef)}
           />
         </div>
-        <div class="nes-field">
+        <div className="nes-field">
           <input
             type="password"
             id="pw_field"
-            class="nes-input"
+            className="nes-input"
             placeholder="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            ref={pwRef}
+            onFocus={() => handleFocus(pwRef)}
           />
         </div>
         <button type="submit" className="nes-btn login-btn">
